72ea6141ef6f105268c6450a59f28c474cd4185a
Stefan Marx <stefan@marx-merten.de>
2024-11-25 09:59:14 +0100

    addns base sentryn

---
+++ src/hooks.server.ts
@@ -0,0 +1,18 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: 'https://78afc87a40fdbc833149e7a53623a449@o4508357880446976.ingest.us.sentry.io/4508358015844352',
+
+	tracesSampleRate: 1.0
+
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: import.meta.env.DEV,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
